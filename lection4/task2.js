d2f826c12f014ab99fa86ad760f9c53106c70e0c
Nazarenko <vyacheslav.nazarenko@simpleone.ru>
2024-05-24 00:50:26 +0300

    firsttry

---
+++ lection4/task2.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let age_1 = '12';
+const age_2 = 18;
+const age_3 = 60;
+
+if (typeof age_1 === 'string') {
+    age_1 = +age_1;
+}
+
+switch (true) {
+  case age_1 < age_2: {
+    console.log('You don’t have access cause your age is ' + age_1 + '. It’s less then ' + age_2);
+    break;
+  }
+  case age_1 >= age_2 && age_1 < age_3: {
+    console.log('Welcome!');
+    break;
+  }
+  case age_1 >= age_3: {
+    console.log('Keep calm and look Culture channel');
+    break;
+  }
+  default: {
+    console.log('Technical work');
+  }
+}
---
+++ lection4/task3.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let age_1 = prompt('Hi there! Enter your age below');
+const age_2 = 18;
+const age_3 = 60;
+
+if (typeof age_1 === 'string') {
+    age_1 = +age_1;
+}
+
+switch (true) {
+  case age_1 < age_2: {
+    alert('You don’t have access cause your age is ' + age_1 + '. It’s less then ' + age_2);
+    break;
+  }
+  case age_1 >= age_2 && age_1 < age_3: {
+    alert('Welcome!');
+    break;
+  }
+  case age_1 >= age_3: {
+    alert('Keep calm and look Culture channel');
+    break;
+  }
+  default: {
+    alert('Technical work');
+  }
+}
