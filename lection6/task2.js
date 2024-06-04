6f076441ee0750e089301a54d2b0e23621c4c854
Nazarenko <vyacheslav.nazarenko@simpleone.ru>
2024-06-04 09:29:15 +0300

    firsttry

---
+++ lection6/task2.js
@@ -0,0 +1,76 @@
+// 1. У вас есть массив названий пицц вашего конкурента. Создайте функцию, которая будет принимать ваш набор названий пицц (массив) 
+//   и возвращать только те, которых нет у конкурента (тоже массив). Если все ваши пиццы есть у конкурента - вернуть null
+//   Пиццы конкурента:
+//   const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+function isMyPizzaUnique(myPizzas) {
+    const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+    let result = [];
+
+    myPizzas.forEach(element => {
+        if (!competitorPizzas.includes(element)) {
+            result.push(element);
+        }
+    });
+
+    return (result.length !== 0 ? result : null);
+}
+
+console.log(isMyPizzaUnique(['4 cheeses', 'hawai', 'Gimalai']));
+
+
+
+// 2. Написать функцию, которая принимает предложение (слова разделенные только пробелами) в качестве параметра и выводит в консоль слово с наибольшим количеством букв. 
+//   Если таких слов несколько - выводит их все.
+
+function longestWord(string) {
+    let wordArray = string.split(' ');
+    
+    wordArray.sort(function (a, b) {
+        return b.length - a.length;
+    });
+
+    for (let i = 1; i < wordArray.length; i++) {
+        if (wordArray[i].length < wordArray[i-1].length) {
+            wordArray.splice(i, wordArray.length-i);
+            break;
+        }
+    };
+    const result = wordArray.join(', ');
+
+    return result;
+}
+
+console.log(longestWord('Caprichosa Diablo Peperonida cheeses hawai'));
+
+// 3. Напишите функцию, которая принимает на вход массив чисел, убирает из него дубликаты и возвращает массив с только уникальными значениями.
+
+function isMyNumbersUnique(myNumbers) {
+    let result = myNumbers;
+
+    result.sort(function (a, b) {
+        return a - b;
+    });
+
+    for (let i = 1; i < result.length; i++) {
+        if (result[i] === result[i-1]) {
+            result.splice(i, 1);
+            i--;
+        }
+    };
+
+    return (result.length !== 0 ? result : null);
+}
+
+console.log(isMyNumbersUnique([1, 3, 1, 8, 8, 22, 45, 1, 8, 44]));
+
+// 4. Написать функцию, которая принимает на вход слово и проверяет, является ли это слово палиндромом
+
+function palindrome(str) {
+    str = str.toLowerCase();
+    const result = (str.split('').reverse().join('') === str) ? `Палиндром` : 'Не палиндром';
+
+    return result;
+  }
+  
+  console.log(palindrome('Abraka-akarba'));
---
+++ lection6/task3.js
@@ -0,0 +1,50 @@
+// 5*. Напишите рекурсивную функцию, которая принимает на вход число и складывает его цифры. 
+//   Если сумма получилась больше 9 - снова сложите цифры. И так пока, сумма не станет меньше либо равной 9. 
+//   После окончания сложений возвращает полученное число. Например при подаче числа 19 (1+9=10>9, потому 1+0=1) выводится 1
+
+function recursion(number) {
+    if (number <= 9) {
+        return number;
+    } else {
+        let sum = 0;
+        const array = number.toString().split('');
+
+        array.forEach(element => {
+            sum = sum + +element;
+        });
+
+        return recursion(sum);
+    }
+  }
+  
+  console.log(recursion(19123));
+
+// 6*. Написать функцию, которая принимает на вход строку с текстом, и заменяет каждую пару стоящих подряд идентичных букв на одну следующую в алфавите, 
+//     и так пока в тексте не останется двух одинаковых букв стоящих рядом (через пробел и другой знак препинания можно)
+//     Пример: aabc => bbc => cc => d
+
+function transformer(string) {  // работает и с верхним регистром
+    let charArray = string.split('');
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    for (let i = 0; i < charArray.length; i++) {
+
+        if (charArray[i] === charArray[i+1] ) {
+            let correctCase = alphabet;
+            if (alphabet.indexOf(charArray[i]) === -1) {
+                correctCase = alphabet.toUpperCase();
+            }
+            let newCharIndex = correctCase.indexOf(charArray[i]) + 1;
+            if (newCharIndex === 26) {
+                newCharIndex = 0;
+            }
+            charArray[i] = correctCase[newCharIndex];
+            charArray.splice(i+1, 1);
+            i--;
+        }
+    };
+
+    return charArray.join('');
+}
+
+console.log(transformer('aabc-ZZA'));
